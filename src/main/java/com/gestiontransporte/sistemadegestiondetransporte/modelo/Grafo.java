0b99526eb8e73e0d34e417521979e63685d56d61
Abel Faringthon <aaft0001@ce.pucmm.edu.do>
2026-03-11 15:25:38 -0300

    Arreglando Clase Grafo y agregando metodos basicos

--- src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
+++ src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
@@ -1,75 +1,31 @@
 package com.gestiontransporte.sistemadegestiondetransporte.modelo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Grafo {
-    private Map<Parada, List<Ruta>> adyacencia;
-    // Costo de memoria con una Lista de Adyacencia es O(V+E) y si fuera mastriz es de O(V^2)
+    List<Parada> paradas = new ArrayList<>();
+    private Map<Integer, List<Ruta>> adyacencia = new HashMap<>();
 
-    public Grafo(Map<Parada, List<Ruta>> adyacencia) {
-        this.adyacencia = adyacencia;
+    public void agregarParada(Parada parada){
+        paradas.add(parada);
+        adyacencia.putIfAbsent(parada.getId(), new ArrayList<>());
     }
 
-    public Map<Parada, List<Ruta>> getAdyacencia() {
-        return adyacencia;
+    public void agregarRuta(Ruta ruta) {
+        int origen = ruta.getOrigen().getId();
+        adyacencia.putIfAbsent(origen, new ArrayList<>());
+        adyacencia.get(origen).add(ruta);
     }
 
-    public void setAdyacencia(Map<Parada, List<Ruta>> adyacencia) {
-        this.adyacencia = adyacencia;
+    public List<Parada> getParadas() {
+        return paradas;
     }
 
-    public boolean agregarParada(Parada parada){
-
-        if(parada == null){
-            return false;
+    public List<Ruta> getTodasLasRutas(){
+        List<Ruta> resultado = new ArrayList<>();
+        for(List<Ruta> list : adyacencia.values()){
+            resultado.addAll(list);
         }
-
-        if( this.adyacencia.containsKey(parada) ) {
-            return false;
-        }
-
-        // ArrayList porque es lo mas eficiente a la hora de recorrer varias veces los caminos
-        adyacencia.put(parada, new ArrayList<>());
-        return true;
-
+        return resultado;
     }
-
-    public boolean borrarParada(Parada parada){
-
-    }
-
-    public boolean agregarRuta(Ruta ruta) {
-
-        if (ruta == null) {
-            return false;
-        }
-
-        if (ruta.getOrigen() == null || ruta.getDestino() == null) {
-            return false;
-        }
-
-        if (!adyacencia.containsKey(ruta.getOrigen()) || !adyacencia.containsKey(ruta.getDestino())) {
-            return false;
-        }
-
-        for (Ruta r : adyacencia.get(ruta.getOrigen())) {
-            if (r.getDestino().equals(ruta.getDestino())) {
-                return false;
-            }
-        }
-
-        adyacencia.get(ruta.getOrigen()).add(ruta);
-        return true;
-    }
-
-    public boolean eliminarRuta(Ruta ruta){
-
-    }
-
-    public void mostrarGrafo(){
-
-    }
-
 }
