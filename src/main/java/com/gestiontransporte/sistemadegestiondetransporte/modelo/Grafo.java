b5876c72415b99b382af90bb5505994caca0477f
wagnerpeterc <wpetercepin@gmail.com>
2026-03-12 01:22:48 -0400

    falta poco del grafo
    Signed-off-by: wagnerpeterc <wpetercepin@gmail.com>

--- src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
+++ src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
@@ -3,13 +3,48 @@ package com.gestiontransporte.sistemadegestiondetransporte.modelo;
 import java.util.*;
 
 public class Grafo {
-    List<Parada> paradas = new ArrayList<>();
-    private Map<Integer, List<Ruta>> adyacencia = new HashMap<>(); //parada.GetId
+    private final List<Parada> paradas = new ArrayList<>();
+    private final Map<Integer, List<Ruta>> adyacencia = new HashMap<>(); //parada.GetId
 
     public void agregarParada(Parada parada){
         paradas.add(parada);
         adyacencia.putIfAbsent(parada.getId(), new ArrayList<>());
     }
+
+    public boolean modificarParada(int id, Parada parada){
+        for (Parada p : paradas){
+            if(p.getId() == id){
+                p.setNombre(parada.getNombre());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void eliminarParada(int id){
+        // eliminar la parada de la lista
+        for(int i = 0; i < paradas.size(); i++){
+            if(paradas.get(i).getId() == id){
+                paradas.remove(i);
+                break;
+            }
+        }
+
+        // eliminar las rutas que salen de esa parada
+        adyacencia.remove(id);
+
+
+        // eliminar las rutas que llegan a esa parada
+        for(List<Ruta> rutas : adyacencia.values()){
+            for(int i = 0; i < rutas.size(); i++){
+                if(rutas.get(i).getDestino().getId() == id){
+                    rutas.remove(i);
+                    i--; // ajustar indice porque la lista retrocedio
+                }
+            }
+        }
+    }
+
 
     public void agregarRuta(Ruta ruta) {
         int origen = ruta.getOrigen().getId();
@@ -17,8 +52,16 @@ public class Grafo {
         adyacencia.get(origen).add(ruta);
     }
 
+    public void modificarRuta(Ruta ruta){}
+
+
+    public void eliminarRuta(Ruta ruta){
+
+    }
+
+
     public List<Parada> getParadas() {
-        return paradas;
+        return new ArrayList<>(paradas);
     }
 
     public List<Ruta> getTodasLasRutas(){
