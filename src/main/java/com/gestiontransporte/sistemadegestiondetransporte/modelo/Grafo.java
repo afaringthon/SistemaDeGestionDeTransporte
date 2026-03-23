31f26d19acd3e805ff7bb9fde7df5ea21c2b736b
Abel Faringthon <aaft0001@ce.pucmm.edu.do>
2026-03-23 15:35:12 -0300

    Corrijiendo Grafo, Agregando Set y lista paradas

--- src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
+++ src/main/java/com/gestiontransporte/sistemadegestiondetransporte/modelo/Grafo.java
@@ -3,191 +3,46 @@ package com.gestiontransporte.sistemadegestiondetransporte.modelo;
 import java.util.*;
 
 public class Grafo {
-    private final Map<Parada, List<Ruta>> adyacencia;
+    private final Map<Integer, Parada> paradas;
+    private final Map<Parada, Set<Ruta>> listaAdj;
 
     public Grafo() {
-        adyacencia = new HashMap<>();
+        this.paradas = new HashMap<>();
+        this.listaAdj = new HashMap<>();
     }
 
-    public boolean agregarParada(Parada parada){
-
-        if(parada == null){
-            return false;
-        }
-
-        if(adyacencia.containsKey(parada)){
-            return false;
-        }
-
-        adyacencia.put(parada, new ArrayList<>());
-        return true;
+    public void agregarParada(Parada parada){
+        paradas.put(parada.getId(), parada);
     }
 
-    public boolean modificarParada(int idViejaParada, Parada nuevaParada){
-
-        if(nuevaParada == null){
-            return false;
-        }
-
-        for(Parada p : adyacencia.keySet()){
-            if(p.getId() == idViejaParada){
-                p.setNombre(nuevaParada.getNombre());
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean eliminarParada(Parada paradaAEliminar){
-
-        if(paradaAEliminar == null || !adyacencia.containsKey(paradaAEliminar)){
-            return false;
-        }
-
-        adyacencia.remove(paradaAEliminar);
-
-        for(List<Ruta> rutas : adyacencia.values()){
-            rutas.removeIf(ruta -> ruta.getDestino().getId() == paradaAEliminar.getId());
-        }
-
-        return true;
-    }
-
-
-    public boolean agregarRuta(Ruta ruta) {
-        if(ruta == null){
-            return false;
-        }
-
+    public boolean agregarRuta(Ruta ruta){
         Parada origen = ruta.getOrigen();
         Parada destino = ruta.getDestino();
 
-        if(origen == null || destino == null){
-            return false;
+        if(origen == null || destino == null) return false;
+
+        Set<Ruta> vecinos = listaAdj.get(origen);
+        if(vecinos == null){
+            vecinos = new HashSet<>();
+            listaAdj.put(origen, vecinos);
         }
-
-        if(!adyacencia.containsKey(origen) || !adyacencia.containsKey(destino)){
-            return false;
-        }
-
-        List<Ruta> rutasOrigen = adyacencia.get(origen);
-
-        //verifica que no haya una ruta entre existente entre las paradas
-        for(Ruta r : rutasOrigen){
-            if(r.getDestino().equals(destino)){
-                return false;
-            }
-        }
-
-        rutasOrigen.add(ruta);
-
-        return true;
+        return vecinos.add(ruta);
     }
 
-    public boolean modificarRuta(Ruta viejaRuta, Ruta nuevaRuta){
+    public void eliminarParada(Parada parada){
+        paradas.remove(parada.getId());
+        listaAdj.remove(parada);
 
-        if(viejaRuta == null || nuevaRuta == null){
-            return false;
+        for(Set<Ruta> rutas : listaAdj.values()){
+            rutas.removeIf(ruta -> ruta.getDestino().equals(parada));
         }
-
-        Parada viejoOrigen = nuevaRuta.getOrigen();
-        Parada viejoDestino = nuevaRuta.getDestino();
-
-        Parada nuevoOrigen = nuevaRuta.getOrigen();
-        Parada nuevoDestino = nuevaRuta.getDestino();
-
-        if( !adyacencia.containsKey(nuevoOrigen) || !adyacencia.containsKey(nuevoDestino) ||
-                !adyacencia.containsKey(viejoOrigen) || !adyacencia.containsKey(viejoDestino)){
-            return false;
-        }
-
-
-
-
-
     }
-
-//Validar todas estas funciones
 
     public void eliminarRuta(Ruta ruta){
-        int idOrigen = ruta.getOrigen().getId();
-        List<Ruta> rutas = adyacencia.get(idOrigen);
-
-        for(int ind = 0 ; ind < rutas.size() ; ind++){
-            if(rutas.get(ind).equals(ruta) ){
-                rutas.remove(ind);
-            }
+        Parada origen = ruta.getOrigen();
+        Set<Ruta> vecinos = listaAdj.get(origen);
+        if(vecinos != null){
+            vecinos.remove(ruta);
         }
-
-    }
-
-    public void crearRutaDoble(Parada o, Parada d, double dist, double tiem) {
-        Ruta ida = new Ruta();
-        ida.setOrigen(o);
-        ida.setDestino(d);
-        ida.setDistancia(dist);
-        ida.setTiempo(tiem);
-        agregarRuta(ida);
-
-        Ruta vuelta = new Ruta();
-        vuelta.setOrigen(d);
-        vuelta.setDestino(o);
-        vuelta.setDistancia(dist);
-        vuelta.setTiempo(tiem);
-        agregarRuta(vuelta);
-    }
-
-    public void eliminarRutaDoble(Parada origen, Parada destino){
-        if(origen == null || destino == null){
-            return;
-        }
-        int idOrigen = origen.getId();
-        int idDestino = destino.getId();
-
-        // eliminar origen a destino
-        List<Ruta> rutasOrigen = adyacencia.get(idOrigen);
-        if(rutasOrigen != null){
-            for(int i = 0; i < rutasOrigen.size(); i++){
-                Ruta r = rutasOrigen.get(i);
-
-                if(r.getDestino().getId() == idDestino){
-                    rutasOrigen.remove(i);
-                    break;
-                }
-            }
-        }
-
-        // eliminar destino a origen
-        List<Ruta> rutasDestino = adyacencia.get(idDestino);
-        if(rutasDestino != null){
-            for(int i = 0; i < rutasDestino.size(); i++){
-                Ruta r = rutasDestino.get(i);
-
-                if(r.getDestino().getId() == idOrigen){
-                    rutasDestino.remove(i);
-                    break;
-                }
-            }
-        }
-    }
-
-
-    public List<Parada> getParadas() {
-        return new ArrayList<>(paradas);
-    }
-
-
-    //Funcion inecesaria, hacer un return simple de todas las listas
-    public List<Ruta> getTodasLasRutas(){
-        List<Ruta> resultado = new ArrayList<>();
-        for(List<Ruta> list : adyacencia.values()){
-            resultado.addAll(list);
-        }
-        return resultado;
-    }
-
-
-    public List<Ruta> getRutasDesde(int idParada) {
-        return adyacencia.getOrDefault(idParada, Collections.emptyList());
     }
 }
